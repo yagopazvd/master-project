c6f51cfde4192a0732e1e818d1fff054b55fdee3
Yago Pereira <yagopazvd@gmail.com>
2019-07-21 16:07:53 -0300

    set up some routes with react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './components/homepage/HomePage.component';
 
+const HatsPage = () => (
+  <h1>HATS PAGE</h1>
+)
+
 function App() {
   return (
     <div >
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/MenuItem.component.jsx
+++ src/components/menu-item/MenuItem.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 import './MenuItem.styles.scss';
 
 const MenuItem = (props) => (
-    <div className={`${props.size} menu-item`}>
-        <div className="background-image" style={{backgroundImage: `url(${props.image})`}} />
+    <div className={`${props.size} menu-item`} onClick={() => props.history.push(`${props.match.url}${props.title}`)}>
+        <div 
+        className="background-image" 
+        style={{backgroundImage: `url(${props.image})`}} 
+        />
         <div className="content">
             <h1 className="title">{props.title.toUpperCase()}</h1>
             <span className="subtitle">SHOP NOW</span>
@@ -11,4 +16,4 @@ const MenuItem = (props) => (
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
