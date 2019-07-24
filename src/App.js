7e665037f4c84c3662d09d5b2a95be7aa675f5c8
Yago Pereira <yagopazvd@gmail.com>
2019-07-24 17:49:39 -0300

    finished sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
               ...snapShot.data()
             }
           })
+          console.log(this.state);
         })
       }
       this.setState({ currentUser: userAuth})
--- src/components/sign-in/SignIn.component.jsx
+++ src/components/sign-in/SignIn.component.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import FormInput from '../form-input/FormInput.component';
 import CustomButton from '../custom-button/CustomButton.component';
 
-import { signInWithGoogle} from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 import './SignIn.styles.scss';
 
@@ -23,8 +23,18 @@ class SignIn extends Component {
 
     }
 
-    handleSubmit = (e) => {
+    handleSubmit = async e => {
         e.preventDefault();
+
+        const {email , password } = this.state
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email:'', password:''})
+        } catch(error) {
+            console.log(error);
+        }
+
 
         this.setState({
             email: "",
